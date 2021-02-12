ba5b9848db86ccd72eff6bbcf341bbe3ee36c286
Sasha Osipova <aosip001@fiu.edu>
2021-02-12 14:31:05 -0500

    update

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "68524e14c88fa47aab3ac62f9461f6d5"
+
+# Google API Key
+g_key = "AIzaSyDF25uqaTZFFTEh1aRCJ7r_oABnl3lpb6g"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "51c74617b3635f026fbcd312f9ba4e7d"
+
+# Google API Key
+g_key = "AIzaSyDF25uqaTZFFTEh1aRCJ7r_oABnl3lpb6g"
